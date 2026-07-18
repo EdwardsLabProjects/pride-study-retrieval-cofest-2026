0317c8b1a3ec6b0044aa1c6fe34bee04772e65e6
Nathan Edwards <edwardsnj@gmail.com>
2026-07-18 10:37:24 -0400

    bug fix

--- notebooks/lib.py
+++ notebooks/lib.py
@@ -4,7 +4,7 @@ GITHUB = "https://raw.githubusercontent.com/EdwardsLabProjects/pride-study-retri
 import os, os.path, subprocess
 import pandas
 
-VERSION='1.0.12'
+VERSION='1.0.13'
 
 def download_embeddings(model="openai-3-small"):
     # files...
@@ -203,7 +203,7 @@ def top_features(model,nembed=0,use_embed=True,use_tfidf=True,**kwargs):
     
     if use_embed:
         # Calculate significant embedding coefficients
-        embedding_coefficients = trained_model.coef_[0][:nembed]
+        embedding_coefficients = model.coef_[0][:nembed]
         significant_embedding_coeffs = np.sum(embedding_coefficients != 0)
         print(f"\nNumber of semantic embedding non-zero coefficients: {significant_embedding_coeffs}")
 
